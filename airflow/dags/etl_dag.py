6a3b9478f531daca62f0196013ec3812901ab0a1
Danilo Freitas <danilojpfreitas@gmail.com>
2023-06-29 02:12:40 +0000

    Finalização

--- airflow/dags/etl_dag.py
+++ airflow/dags/etl_dag.py
@@ -28,16 +28,14 @@ spark = SparkSession \
         .getOrCreate()
 
 #MySql
-dbOptions = {
-    "driver" : "com.mysql.cj.jdbc.Driver",
-    "url" : "jdbc:mysql://172.18.0.2:3306",
-    "user" : "mesha",
-    "password" : "mesha"
-}
+url = "172.19.0.2:3306/mesha" #000.00.0.0:0000/database
+driver = "com.mysql.cj.jdbc.Driver"
+user = "mesha"
+password = "mesha"
 
 pathRow = "/opt/airflow/dados/raw/DADOS/MICRODADOS_ENEM_2020.csv"
 pathParquetProcessing = "/opt/airflow/dados/processing"
-
+pathCurated = "/opt/airflow/dados/curated"
 
 def extracao_dados():
 
@@ -115,15 +113,17 @@ def extracao_dados():
     mode = "overwrite"
     )
 
-def curated_zone_pergunta_01():
+# Criação da View para as próximas DAGs
+enemParquet = spark.read.parquet(
+pathParquetProcessing
+)
+
+enemParquet.createOrReplaceTempView("enemView")
+
+
+def pergunta_01_curated_mysql():
 
     # 1 - Qual a escola com a maior média de notas?
-
-    enemParquet = spark.read.parquet(
-    pathParquetProcessing
-    )
-
-    enemParquet.createOrReplaceTempView("enemView")
 
     escolaMaiorMedia = spark.sql("""
         SELECT 
@@ -154,22 +154,344 @@ def curated_zone_pergunta_01():
         ORDER BY MediaNotas DESC
         """)
 
-    escolaMaiorMedia.limit(10).show()
-
-    # escolaMaiorMedia.write\
-    #     .format("jdbc")\
-    #     .option(**dbOptions)\
-    #     .option("dbtable", "escolaMaiorMedia")\
-    #     .save()
+    escolaMaiorMedia = escolaMaiorMedia.limit(1)
 
     escolaMaiorMedia.write\
-        .format("jdbc")\
-        .option("driver","com.mysql.cj.jdbc.Driver")\
-        .option("url", "jdbc:mysql://172.19.0.2:3306/mesha")\
-        .option("dbtable", "mesha")\
-        .option("user", "mesha")\
-        .option("password", "mesha")\
-        .save()
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "01escolamaiormedia")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_02_curated_mysql():
+
+    # 2 - Qual o aluno com a maior média de notas e o valor dessa média?
+
+    # Dataframe and View Etnia
+    etniaColumns = ["codEtnia", "nomeEtnia"]
+    etniaNome = [(0, "Nao declarado"), (1, "Branca"), (2, "Preta"), (3, "Parda"), (4, "Amarela"), (5, "Indigena")]
+    etniaDataframe = spark.createDataFrame(etniaNome, etniaColumns)
+    etniaDataframe.show()
+
+    etniaDataframe.createOrReplaceTempView("etniaView")
+
+    # Dataframe and View SituacaoConclusao
+    sitConColumns = ["codCon", "nomeCon"]
+    sitConNome = [(1, "Concluido"), (2, "Cursando/Conclusao 2020"), (3, "Cursando/Conclusao Apos 2020"), (4, "Sem Conclusao/Previsao")]
+    sitConDataframe = spark.createDataFrame(sitConNome, sitConColumns)
+    sitConDataframe.show()
+
+    sitConDataframe.createOrReplaceTempView("sitConView")
+
+    # Join Tables Etnia e SituaçãoConclusão
+    alunoMaiorNota = spark.sql("""
+        SELECT
+            A.NumInscricao, A.Sexo,
+            B.nomeEtnia AS Etnia,
+            A.NomMunicipioEsc, A.EstadoEsc,
+            C.nomeCon AS SituacaoConclusao,
+            A.MediaNotas
+        FROM
+            (SELECT
+                NumInscricao, Sexo, 
+                Etnia, NomMunicipioEsc, 
+                EstadoEsc, 
+                SituacaoConclusao,
+                ROUND((NotaCN+NotaCH+NotaLC+NotaMT+NotaRedacao)/5, 2) AS MediaNotas
+            FROM
+                enemView
+            WHERE 
+                    NotaMT IS NOT NULL
+                AND
+                    NotaCH IS NOT NULL
+                AND
+                    NotaCN IS NOT NULL
+                AND
+                    NotaLC IS NOT NULL
+                AND 
+                    NotaRedacao IS NOT NULL) A
+        INNER JOIN
+            etniaView B
+            ON codEtnia == Etnia
+        INNER JOIN
+            sitConView C
+            ON codCon == SituacaoConclusao
+        ORDER BY MediaNotas DESC
+        """)
+
+    alunoMaiorNota = alunoMaiorNota.select("NumInscricao", "Sexo", "Etnia", "SituacaoConclusao", "MediaNotas").limit(1)
+
+    alunoMaiorNota.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "02alunomaiornota")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_03_curated_mysql():
+
+    # 3 - Qual a média geral?
+
+    mediaGeral = spark.sql("""
+        SELECT 
+            ROUND(AVG(ROUND((NotaCN+NotaCH+NotaLC+NotaMT+NotaRedacao)/5, 2)), 2) AS MediaGeral
+        FROM
+            (SELECT 
+                *
+            FROM
+                enemView
+            WHERE
+                    NotaMT IS NOT NULL
+                AND
+                    NotaCH IS NOT NULL
+                AND
+                    NotaCN IS NOT NULL
+                AND
+                    NotaLC IS NOT NULL
+                AND 
+                    NotaRedacao IS NOT NULL
+            )
+        """)
+
+    mediaGeral.limit(1).show()
+
+    mediaGeral.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "03mediageral")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_04_curated_mysql():
+
+    # 4 - Qual o % de Ausentes?
+
+    ausentesEnem = spark.sql("""
+            SELECT
+                COUNT(NumInscricao) AS NumAusentes
+            FROM
+                enemView
+            WHERE 
+                    PresencaCN = 0
+                OR
+                    PresencaCH = 0
+                OR
+                    PresencaLC = 0
+                OR
+                    PresencaMT = 0
+        """)
+
+    totalEnem = spark.sql("""
+        SELECT
+            COUNT(NumInscricao) AS NumTotal
+        FROM
+            enemView
+        """)
+    
+    ausentesEnem.createOrReplaceTempView("ausentesEnemView")
+
+    totalEnem.createOrReplaceTempView("totalEnemView")
+
+    porcentagemAusentesEnem = spark.sql("""
+        SELECT
+            B.NumTotal, A.NumAusentes, 
+            ROUND(((A.NumAusentes*100)/B.NumTotal), 2) AS PorcentagemAusentes
+        FROM
+            ausentesEnemView AS A,
+            totalEnemView AS B
+        """)
+
+    porcentagemAusentesEnem.show()
+
+    porcentagemAusentesEnem.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "04porceausentes")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_05_curated_mysql():
+
+    # 5 - Qual o número total de Inscritos?
+
+    totalInscritos = spark.sql("""
+        SELECT
+            COUNT(NumInscricao) AS TotalInscritos
+        FROM
+            enemView 
+        """)
+    
+    totalInscritos.show()
+
+    totalInscritos.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "05totalinscritos")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_06_curated_mysql():
+
+    # 6 - Qual a média por disciplina?
+
+    mediaDisciplinas = spark.sql("""
+        SELECT
+            A.MediaCN,
+            B.MediaCH,
+            C.MediaLC,
+            D.MediaMT,
+            E.MediaRedacao
+        FROM
+            (SELECT
+                ROUND(AVG(NotaCN), 2) AS MediaCN
+            FROM
+                enemView
+            WHERE
+                NotaCN IS NOT NULL
+            ) AS A,
+            (SELECT
+                ROUND(AVG(NotaCH), 2) AS MediaCH
+            FROM
+                enemView
+            WHERE
+                NotaCH IS NOT NULL
+            ) AS B,
+            (SELECT
+                ROUND(AVG(NotaLC), 2) AS MediaLC
+            FROM
+                enemView
+            WHERE
+                NotaLC IS NOT NULL
+            ) AS C,
+            (SELECT
+                ROUND(AVG(NotaMT), 2) AS MediaMT
+            FROM
+                enemView
+            WHERE
+                NotaMT IS NOT NULL
+            ) AS D,
+            (SELECT
+                ROUND(AVG(NotaRedacao), 2) AS MediaRedacao
+            FROM
+                enemView
+            WHERE
+                NotaRedacao IS NOT NULL
+            ) AS E
+        """)
+
+    mediaDisciplinas.show()
+
+    mediaDisciplinas.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "06mediadisciplinas")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+    
+
+def pergunta_07_curated_mysql():
+
+    # 7 - Qual a média por Sexo?
+
+    mediaSexo = spark.sql("""
+        SELECT
+            Sexo,
+            ROUND(AVG(((NotaCN+NotaCH+NotaLC+NotaMT+NotaRedacao)/5)), 2) AS MediaNotaSexo
+        FROM
+            enemView
+        WHERE 
+                NotaMT IS NOT NULL
+            AND
+                NotaCH IS NOT NULL
+            AND
+                NotaCN IS NOT NULL
+            AND
+                NotaLC IS NOT NULL
+            AND 
+                NotaRedacao IS NOT NULL
+        GROUP BY Sexo
+        """)
+
+    mediaSexo.show()
+
+    mediaSexo.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "07mediasexo")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
+
+
+def pergunta_08_curated_mysql():
+
+    # 8 - Qual a média por Etnia?
+
+    # Dataframe and View Etnia
+
+    etniaColumns = ["codEtnia", "nomeEtnia"]
+    etniaNome = [(0, "Nao declarado"), (1, "Branca"), (2, "Preta"), (3, "Parda"), (4, "Amarela"), (5, "Indigena")]
+    etniaDataframe = spark.createDataFrame(etniaNome, etniaColumns)
+    etniaDataframe.show()
+
+    etniaDataframe.createOrReplaceTempView("etniaView")
+
+    #Selecao
+
+    mediaEtnia = spark.sql("""
+        SELECT
+            B.nomeEtnia AS Etnia,
+            A.MediaNotaEtnia
+        FROM
+            (SELECT
+                Etnia,
+                ROUND(AVG(((NotaCN+NotaCH+NotaLC+NotaMT+NotaRedacao)/5)), 2) AS MediaNotaEtnia
+            FROM
+                enemView
+            WHERE 
+                    NotaMT IS NOT NULL
+                AND
+                    NotaCH IS NOT NULL
+                AND
+                    NotaCN IS NOT NULL
+                AND
+                    NotaLC IS NOT NULL
+                AND 
+                    NotaRedacao IS NOT NULL
+            GROUP BY Etnia) A
+        INNER JOIN
+            etniaView B
+        ON codEtnia == Etnia
+        """)
+
+    mediaEtnia.show()
+
+    mediaEtnia.write\
+    .format("jdbc")\
+    .option("driver",f"{driver}")\
+    .option("url", f"jdbc:mysql://{url}")\
+    .option("dbtable", "08mediaetnia")\
+    .option("user", f"{user}")\
+    .option("password", f"{password}")\
+    .save()
 
 
 extracao_dados = PythonOperator(
@@ -179,14 +501,63 @@ extracao_dados = PythonOperator(
   dag=dag
 )
 
-curated_zone_pergunta_01 = PythonOperator(
-  task_id='curated_zone_pergunta_01',
+pergunta_01_curated_mysql = PythonOperator(
+  task_id='pergunta_01_curated_mysql',
   provide_context=True,
-  python_callable=curated_zone_pergunta_01,
+  python_callable=pergunta_01_curated_mysql,
   dag=dag
 )
 
-extracao_dados >> curated_zone_pergunta_01
+pergunta_02_curated_mysql = PythonOperator(
+  task_id='pergunta_02_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_02_curated_mysql,
+  dag=dag
+)
+
+pergunta_03_curated_mysql = PythonOperator(
+  task_id='pergunta_03_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_03_curated_mysql,
+  dag=dag
+)
+
+pergunta_04_curated_mysql = PythonOperator(
+  task_id='pergunta_04_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_04_curated_mysql,
+  dag=dag
+)
+
+pergunta_05_curated_mysql = PythonOperator(
+  task_id='pergunta_05_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_05_curated_mysql,
+  dag=dag
+)
+
+pergunta_06_curated_mysql = PythonOperator(
+  task_id='pergunta_06_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_06_curated_mysql,
+  dag=dag
+)
+
+pergunta_07_curated_mysql = PythonOperator(
+  task_id='pergunta_07_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_07_curated_mysql,
+  dag=dag
+)
+
+pergunta_08_curated_mysql = PythonOperator(
+  task_id='pergunta_08_curated_mysql',
+  provide_context=True,
+  python_callable=pergunta_08_curated_mysql,
+  dag=dag
+)
+
+extracao_dados >> [pergunta_01_curated_mysql, pergunta_02_curated_mysql, pergunta_03_curated_mysql, pergunta_04_curated_mysql, pergunta_05_curated_mysql, pergunta_06_curated_mysql, pergunta_07_curated_mysql, pergunta_08_curated_mysql]
 
 
 
